98186898ea617cae7dc8942d884a359082c02aad
Zachary Porter <zachary.s.porter@outlook.com>
2022-03-20 14:16:35 -0500

    init

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello, World')
+import time
+import numpy as np
+import sys
+
+# Print the text slowly with a delay print like gameboy
+def delay_print(self, str):
+  for char in str:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.05)
+
+
+
+if __name__ == '__main__':
+  pass
